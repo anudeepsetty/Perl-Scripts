152372a2f3d3390ce32fb66f2ea8e5693ef5bf89
anudeepsetty <103600304+anudeepsetty@users.noreply.github.com>
2026-05-08 14:52:58 -0700

    Create setAllocationAll.pl

---
+++ setAllocationAll.pl
@@ -0,0 +1,41 @@
+#!/bin/perl
+
+$cluster = shift(@ARGV);
+
+$user     = "";
+$password = "";
+$base     = "/apps/c1";
+open(IN, "$base/scripts/clusters.cfg");
+@clusters = <IN>
+close IN;
+
+foreach $line(@clusters)
+{
+        chomp $line;
+        ($clusterName,$httpHostName) = split(/\|/,$line);
+        $clusterName =~ tr/[A-Z]/[a-z]/;
+        $httpHostLookup{$clusterName} = $httpHostName;
+        push(@clusterList,$clusterName);
+}
+
+$cluster =~ tr/[A-Z]/[a-z]/;
+$clusterList = join('|', @clusterList);
+
+if($cluster eq "")
+{
+        print "usage clusterHealth.pl [$clusterList]\n";
+        exit;
+}
+
+$httpsHost = $httpHostLookup{$cluster};
+$date = `date --date=$numDays days ago" + "%Y.%m.%d"`;
+chomp $date;
+
+@curl = `curl -XPUT -k -u $user:$password -H "Content-Type: application/json" "https://$httpsHost:9200/_cluster/settings" -d '
+{
+  "persistent": {
+    "cluster.routing.allocation.enable": "all"
+    }
+}'   ;
+
+print @curl;
